a371892507a9a345ce2a34a57dd52d8757d3d158
luoluodabuguo <3510198433@qq.com>
2026-09-01 19:48:20 +0800

    Fix audio source path in _createAudio function

--- js/features.js
+++ js/features.js
@@ -194,7 +194,8 @@
 
     function _createAudio() {
         if (_audio) return _audio;
-        _audio = new Audio(SRC);
+        _audio = new Audio( milkhoutaibaohuo
+                          .m4a);
         _audio.loop   = true;
         _audio.volume = 0.01;
         _audio.preload = 'auto';
